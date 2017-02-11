772e42f76407a4f5f285ed9bdbc04aea1bcecd1f
Gagan Malhotra <gaganmalhotra16@gmail.com>
2017-02-11 15:36:39 -0500

    init gagan

--- server/db.js
+++ server/db.js
@@ -1,6 +1,6 @@
 var PouchDB = require('pouchdb');
 
-var db = new PouchDB('flightInfo');
+var db = new PouchDB('http://localhost:5984/flightss');
 
 
 
@@ -18,20 +18,29 @@ var getFlightsInfo = function () {
 }
 
 
-db.get('sampleFlightInfo').then(function(doc) {
-  return db.put({
-    _id: 'sampleFlightInfo',
-    _rev: doc._rev,
-    title: "Let's Dance"
-  });
-}).then(function(response) {
-  console.log(response);
-
-
-  // handle response
-}).catch(function (err) {
-  console.log(err);
+//Database information
+db.info(function(err, info) {
+   if (err) {
+      return console.log(err);
+   } else {
+      console.log(info);
+   }
 });
 
+// db.get('sampleFlightInfo').then(function(doc) {
+//   return db.put({
+//     _id: 'sampleFlightInfo',
+//     _rev: doc._rev,
+//     title: "Let's Dance"
+//   });
+// }).then(function(response) {
+//   console.log(response);
+//
+//
+//   // handle response
+// }).catch(function (err) {
+//   console.log(err);
+// });
+//
 
 //getFlightsInfo();
